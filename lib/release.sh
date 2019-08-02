dc842f560e661fb6f88833b338c24c820ddeba5e
MJC <mjc@mjczone.com>
2019-08-02 16:34:02 -0500

    script updates

--- lib/release.sh
+++ lib/release.sh
@@ -30,6 +30,8 @@ git push
 git tag -a v$VERSION -m "Release of version v$VERSION"
 git push --tags
 
+echo Creating release v$VERSION
+
 # create release on GitHub
 API_JSON=$(printf '{"tag_name": "v%s","target_commitish": "master","name": "v%s","body": "Release of version %s","draft": false,"prerelease": false}' $VERSION $VERSION $VERSION)
 curl --data "$API_JSON" "https://api.github.com/repos/$GITHUB_REPOSITORY/releases?access_token=$GITHUB_ACCESSTOKEN"
